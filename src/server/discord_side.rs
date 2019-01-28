e137e748811965e96fed4d8145a5c332666e5840
Michael Zhang <iptq@protonmail.com>
2019-01-28 13:53:19 -0600

    deserialize mentions

--- src/server/discord_side.rs
+++ src/server/discord_side.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::{iter::once, sync::Arc, thread::spawn};
+
+use antidote::RwLock;
 use failure::{format_err, Error, SyncFailure};
 use futures::{
     future::{err, Either, Future},
@@ -7,6 +11,7 @@ use futures::{
     },
     Stream,
 };
+use lazy_static::lazy_static;
 use log::error;
 use serenity::{
     client::{Client, Context, EventHandler},
@@ -14,13 +19,16 @@ use serenity::{
         channel::Message,
         gateway::Ready,
         id::{ChannelId, UserId},
+        prelude::Member,
     },
 };
-use std::{
-    iter::once,
-    sync::{Arc, RwLock},
-    thread::spawn,
-};
+
+use crate::server::discord_parser::get_content;
+
+lazy_static! {
+    pub static ref ID_TO_NICK: Arc<RwLock<HashMap<u64, String>>> =
+        Arc::new(RwLock::new(HashMap::new()));
+}
 
 /// Starts listening for Discord messages, communicating over the given channels.
 pub fn start_discord(
@@ -68,17 +76,19 @@ struct Handler(UnboundedSender<(u64, String, String)>, RwLock<UserId>);
 
 impl EventHandler for Handler {
     fn ready(&self, _ctx: Context, ready: Ready) {
-        *self.1.write().unwrap() = ready.user.id;
+        *self.1.write() = ready.user.id;
     }
 
     fn message(&self, ctx: Context, msg: Message) {
-        if msg.author.id == *self.1.read().unwrap() {
+        if msg.author.id == *self.1.read() {
             return;
         }
 
+        let content = get_content(&msg);
+        println!("content = {}", content);
         let chan_id = msg.channel_id.0;
         let author = msg.author.name;
-        let iter = once((chan_id, author.clone(), msg.content)).chain(
+        let iter = once((chan_id, author.clone(), content)).chain(
             msg.attachments
                 .iter()
                 .map(|a| (chan_id, author.clone(), a.url.clone())),
@@ -90,5 +100,14 @@ impl EventHandler for Handler {
                 break;
             }
         }
+    }
+
+    fn guild_member_update(&self, _: Context, _: Option<Member>, new: Member) {
+        let user = new.user.read();
+        let mut map = ID_TO_NICK.write();
+        map.insert(
+            *user.id.as_u64(),
+            new.nick.unwrap_or_else(|| user.name.clone()),
+        );
     }
 }
